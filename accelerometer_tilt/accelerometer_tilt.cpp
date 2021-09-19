b6c63e068857f95e1d7315a58c02ed2566d16fbe
Mikko Tikkakoski <mikko.tikkakoski@gmail.com>
2021-09-19 11:31:01 +0300

    accelerometer_tilt with MPU9250

--- accelerometer_tilt/accelerometer_tilt.cpp
+++ accelerometer_tilt/accelerometer_tilt.cpp
@@ -2,8 +2,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 #include <SparkFun_ADXL345.h>
-
-#define LCD
+#include <SparkFunMPU9250-DMP.h>
 
 //#define RELEASE_BOARD
 #if defined RELEASE_BOARD
@@ -21,6 +20,7 @@ const uint8_t RESET = 12;
 #endif
 Adafruit_PCD8544 display = Adafruit_PCD8544(CLK, DIN, DC, CE, RESET);
 
+#ifdef ADXL345
 ADXL345 acc;
 void initAdxl345()
 {
@@ -31,10 +31,27 @@ void initAdxl345()
 	                                      // Higher Values = Wider Measurement Range
 	                                      // Lower Values = Greater Sensitivity
 }
+#else
+MPU9250_DMP imu;
+void initMpu9250()
+{
+	if (imu.begin() != INV_SUCCESS)
+	{
+		display.setCursor(0, 20);
+		display.print("imu failure");
+		display.display();
+		delay(5000);
+		return;
+	}
+	imu.setSensors(INV_XYZ_ACCEL);
+	imu.setAccelFSR(4);
+}
+#endif
+
+
 void setup()
 {
   Serial.begin(115200);
-#ifdef LCD
   display.begin();
   display.setContrast(58);
   display.clearDisplay();
@@ -42,34 +59,15 @@ void setup()
   display.setTextSize(1);
   display.setRotation(2);
   display.setCursor(0,0); display.print("accelerometer_tilt begins"); display.display();
-#endif
-  delay(1000);
-#ifdef LCD
-  display.clearDisplay();
-  display.setCursor(0,0); display.print("connecting to wifi"); display.display();
-#endif
-  //serveOtaFlashPeriod();
-  //startWiFi();
-  //udpConnect();
+  delay(3000);
 
+#ifdef ADXL345
   initAdxl345();
+#else
+  initMpu9250();
+#endif
 
-#ifdef LCD
   display.setCursor(0,20); display.print("imu begin done"); display.display();
-#endif
-  Serial.println("imu begin done");
-  delay(1000);
-
-#if 0
-  imu.dmpBegin(DMP_FEATURE_6X_LP_QUAT | // Enable 6-axis quat
-               DMP_FEATURE_GYRO_CAL, // Use gyro calibration
-              10); // Set DMP FIFO rate to 10 Hz
-  // DMP_FEATURE_LP_QUAT can also be used. It uses the
-  // accelerometer in low-power mode to estimate quat's.
-  // DMP_FEATURE_LP_QUAT and 6X_LP_QUAT are mutually exclusive
-#endif
-
-  Serial.println("setup done!");
   delay(1000);
 }
 
@@ -77,34 +75,47 @@ struct TiltValues
 {
 	double roll{0.0f};
 	double pitch{0.0f};
+	double yaw{0.0f};
 };
-double fXg, fYg, fZg;
 typedef unsigned long TIME;
+
+double fXg, fYg, fZg;
 TiltValues getTiltValues()
 {
    TiltValues values;
    double pitch, roll = 0;
    int Xg, Yg, Zg = 0;
+#ifdef ADXL345
    acc.readAccel(&Xg, &Yg, &Zg);
-   //Low Pass Filter
-   const float alpha = 0.5;
-   fXg = Xg * alpha + (fXg * (1.0 - alpha));
-   fYg = Yg * alpha + (fYg * (1.0 - alpha));
-   fZg = Zg * alpha + (fZg * (1.0 - alpha));
-   //Roll & Pitch Equations
-   values.roll = (atan2(-fYg, fZg)*180.0)/M_PI;
-   values.pitch = (atan2(fXg, sqrt(fYg*fYg + fZg*fZg))*180.0)/M_PI;
-   return values;
+#else
+	if ( imu.dataReady() )
+	{
+	    // Use dmpUpdateFifo to update the ax, gx, mx, etc. values
+		imu.update(UPDATE_ACCEL);
+	}
+	Xg = imu.ax;
+	Yg = imu.ay;
+	Zg = imu.az;
+#endif
+	//Low Pass Filter
+	const float alpha = 0.5;
+	fXg = Xg * alpha + (fXg * (1.0 - alpha));
+	fYg = Yg * alpha + (fYg * (1.0 - alpha));
+	fZg = Zg * alpha + (fZg * (1.0 - alpha));
+	//Roll & Pitch Equations
+	values.roll = (atan2(-fYg, fZg) * 180.0) / M_PI;
+	values.pitch = (atan2(fXg, sqrt(fYg * fYg + fZg * fZg)) * 180.0) / M_PI;
+	return values;
 }
 
 TIME const timeLogThreshold = 200;
 TIME timePrevLog = 0;
 void loop()
 {
-	TiltValues tilt = getTiltValues();
 	TIME const timeNow = millis();
 	if (timeNow - timePrevLog > timeLogThreshold)
 	{
+		TiltValues tilt = getTiltValues();
 		//Serial.print("still alive: "); Serial.println(timeNow);
 		display.clearDisplay();
 		display.display();
@@ -114,6 +125,8 @@ void loop()
         display.print(tilt.pitch);
         display.setCursor(0, 1*8*textSize);
         display.print(tilt.roll);
+        display.setCursor(0, 2*8*textSize);
+        display.print(tilt.yaw);
         display.display();
 		timePrevLog = timeNow;
 	}
